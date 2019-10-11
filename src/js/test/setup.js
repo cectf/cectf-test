74b17ebd48329be1f6ff8700364bcf27f0e2fcda
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-10-10 21:15:18 -0400

    debug logs for travis

--- src/js/test/setup.js
+++ src/js/test/setup.js
@@ -6,8 +6,11 @@ global.nightmare = null;
 
 setupNightmare = function (before, after) {
     beforeAll(() => {
+        console.log("BeforeAll!");
         nightmare = new Nightmare({ show: false });
+        console.log("nightmare'd. sp00ky");
         nightmare.goto('http://ctf-staging.chiquito.us/');
+        console.log("goto san");
         if (before) {
             return before(nightmare);
         }
