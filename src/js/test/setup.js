c0b9678e8c8bb8f82933897dec0e8f40fb278257
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-10-11 16:50:36 -0400

    trying more things

--- src/js/test/setup.js
+++ src/js/test/setup.js
@@ -9,7 +9,7 @@ setupNightmare = function (before, after) {
         console.log("BeforeAll!");
         nightmare = new Nightmare({ show: false });
         console.log("nightmare'd. sp00ky");
-        nightmare.goto('http://ctf-staging.chiquito.us/');
+        nightmare.goto('http://ctf-staging.chiquito.us');
         console.log("goto san");
         if (before) {
             return before(nightmare);
